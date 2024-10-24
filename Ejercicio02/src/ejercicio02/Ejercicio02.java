be982bb7bb57023c66bf804a15a65993cbab2bdf
Paulo Bustamante <pbustamante@utpl.edu.ec>
2024-10-24 11:21:12 -0500

    Actualizacion

--- Ejercicio02/src/ejercicio02/Ejercicio02.java
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -18,7 +18,8 @@ public class Ejercicio02 {
         // Resolución del problema
         int var1 = 10;
         int var2 = 2;
-        
+        System.out.println(Math.pow(var1, var2)+ var1);
+                
                 
     }
     
--- Ejercicio03/src/ejercicio03/Ejercicio03.java
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -16,7 +16,14 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+     int var1 = 10;
+     int var2 = 2;
+     int var3 = 16;
+     int var4 = 20;
+     int var5 = 1;
+     int var6 = 11;
+     double resultado = Math.pow(var1, var2) + Math.sqrt(var3) + (var4 /(var5 + var6));
+     System.out.println(resultado);
     }
     
 }
